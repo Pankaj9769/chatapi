85a9042801c5536e7b5be6590000239b8dfafcd2
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-12-22 02:05:57 +0530

    added cors origin

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ connect();
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173/", "http://localhost:5173"],
+    origin: ["http://localhost:5173/"],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
